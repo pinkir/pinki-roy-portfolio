0f4416cf7b6a15d9e50dc3438db0c78fa59f1a99
pinkir <pinki.roy@outlook.com>
2023-07-31 23:59:41 +0600

    map added

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -36,21 +36,24 @@ const Contact = () => {
                 <p className="text-center text-neutral-500 mb-10 "> For any query feel free to contact</p>
             </div>
 
+
             <div className='col lg:flex justify-center gap-10 '>
-                <div className='drop-shadow-lg'>
-                    <img className='h-[300px]   ' src={contact} alt="" />
+                <div>
+                    {/* <img className='h-[300px]   ' src={contact} alt="" /> */}
+                    <iframe width="520" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=300&amp;hl=en&amp;q=dhaka,%20bangladesh+(Pinki%20Roy)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Calculate population in area</a></iframe>
                 </div>
+
                 <div className=' text-white'>
-                    <h3 className="text-3xl font-bold">Message Me Here!!</h3>
+                    <h3 className="text-3xl font-bold mt-5">Message Me Here!!</h3>
                     <form ref={form} onSubmit={sendEmail} className='text-black'>
-                        <input className='mb-3 mt-3 p-2 w-full rounded' type='text' placeholder="Name" name='name' required />
+                        <input className='mb-3 mt-3 p-2 w-full rounded' type='text' placeholder="Your Name" name='name' required />
                         <br />
-                        <input className='mb-3 p-2 w-full rounded' type='email' placeholder="Email" name='email' required />
+                        <input className='mb-3 p-2 w-full rounded' type='email' placeholder="Your Email" name='email' required />
                         <br />
 
                         <textarea className='p-2 w-full rounded' rows={4} placeholder='Message' name='message' required />
-                        <button className='mt-3 bg-cyan-400 rounded p-2 text-slate-800' type="submit"  name='submit' >
-                            Submit
+                        <button className='mt-3 bg-cyan-400 rounded p-2 text-slate-800' type="submit" name='submit' >
+                            Send
                         </button>
                     </form>
 
--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -8,12 +8,14 @@ const Header = () => {
         <div>
             <nav className='col lg:flex justify-around items-center p-5 text-white bg-black bg-opacity-50 fixed z-10 w-full mx-auto'>
                 <Link to='home'><h3 className='text-3xl font-bold'>PINKI ROY</h3></Link>
-                <div className='text-base'>
+                <div className='text-sm'>
                     <Link to='home' spy={true} smooth={true} offset={-100} duration={500}  className='mr-4'>Home</Link>
                     <Link to='about' spy={true} smooth={true} offset={-100} duration={500} className='mr-4'>About</Link>
                     <Link to='skills' spy={true} smooth={true} offset={-100} duration={500}  className='mr-4'>Skills</Link>
                     <Link to='projects' spy={true} smooth={true} offset={50} duration={500}  className='mr-4'>Projects</Link>
+                    <Link to='contact' spy={true} smooth={true} offset={50} duration={500}  className='mr-4'>Contact</Link>
                     <Link to='blog' spy={true} smooth={true} offset={50} duration={500}  className='mr-4'>Blog</Link>
+                    
                 </div>
 
             </nav>
